f43eb3cd284b94256404abb06c40381e23ffab4d
Adesoba Charles <yomiadesoba@gmail.com>
2022-12-23 16:45:28 +0100

    integrated results from Geolocation Api with map

--- src/App.js
+++ src/App.js
@@ -6,32 +6,45 @@ import Map from "./Map.jsx";
 import SearchIcon from "./search.svg";
 
 const IPGeolocationAPI_URL =
-  "https://geo.ipify.org/api/v2/country?apiKey=at_0c66UA3HLK61AmV0N9mZepyBSWmFh";
-
-// const IPAddress = " 8.8.8.8";
+  "https://geo.ipify.org/api/v2/country,city?apiKey=at_0c66UA3HLK61AmV0N9mZepyBSWmFh";
 
 const getIPInfo = async (IPAddress) => {
   const response = await fetch(
-    `${IPGeolocationAPI_URL}${IPAddress ? `&${IPAddress}` : ""}`
+    `${IPGeolocationAPI_URL}${IPAddress ? `&ipAddress=${IPAddress}` : ""}`
   );
 
   const IPInfo = await response.json();
 
   console.log(IPInfo);
+
+  return IPInfo;
 };
 
-// const getMappingInfo = async () => {};
-
 const App = () => {
-  // useEffect(() => {
-  //   getIPInfo("8.8.8.8");
-  // }, []);
-
   const [searchTerm, setSearchTerm] = useState("");
+
+  const [IPCoordinates, setIPCoordinates] = useState("");
+
+  const performSearch = async (searchTerm) => {
+    const IPInfo = await getIPInfo(searchTerm);
+
+    const IPCoordinates = [IPInfo.location.lat, IPInfo.location.lng];
+
+    setIPCoordinates(IPCoordinates);
+  };
+
+  useEffect(() => {
+    performSearch("");
+  }, []);
 
   return (
     <>
-      <form onSubmit={() => {}}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          performSearch(searchTerm);
+        }}
+      >
         <input
           placeholder="Search IP Address"
           value={searchTerm}
@@ -43,9 +56,17 @@ const App = () => {
         <input type="image" alt="search" src={SearchIcon} />
       </form>
       <h2>Set up Successful</h2>
-      <Map />
+      {IPCoordinates !== "" ? (
+        <Map coordinates={IPCoordinates} />
+      ) : (
+        <p>loading map ...</p>
+      )}
     </>
   );
 };
 
 export default App;
+
+// TODO: look into asynchronous rendering and conditional rendering so as to render the users location on first entering the site
+// TODO: THE USER SHOULDN'T HAVE TO WONDER WHETHER HIS/HER request has been registered --- loading bars for loading maps and resources
+// TODO: auto panning or zooming of map to auto display new coordinates
--- src/Map.jsx
+++ src/Map.jsx
@@ -5,15 +5,15 @@ import './Map.css'
 
 import { MapContainer,TileLayer, useMap, Marker,Popup } from "react-leaflet";
 
-const Map = ({ location }) => {
+const Map = ({ coordinates }) => {
   return (
     <div id="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={coordinates} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={coordinates}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
