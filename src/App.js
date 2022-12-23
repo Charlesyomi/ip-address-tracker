58fa324fdd4e4b6959339d910d3dd6893a687636
Adesoba Charles <yomiadesoba@gmail.com>
2022-12-23 06:59:34 +0100

    added search form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Map from "./Map.jsx";
+
+import SearchIcon from "./search.svg";
 
 const IPGeolocationAPI_URL =
   "https://geo.ipify.org/api/v2/country?apiKey=at_0c66UA3HLK61AmV0N9mZepyBSWmFh";
@@ -25,8 +27,21 @@ const App = () => {
   //   getIPInfo("8.8.8.8");
   // }, []);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
+      <form onSubmit={() => {}}>
+        <input
+          placeholder="Search IP Address"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+
+        <input type="image" alt="search" src={SearchIcon} />
+      </form>
       <h2>Set up Successful</h2>
       <Map />
     </>
